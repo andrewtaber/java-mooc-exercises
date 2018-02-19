039cee7f02265a8d680d2814c68da5b2dd65dc0a
Andrew Taber <andrewtaber@me.com>
2018-02-19 16:44:10 -0700

    Quick exercise in reading from/writing to files.

--- avansbreda-programmeren-2/week11-week11_34.FileManager/src/FileManager.java
+++ avansbreda-programmeren-2/week11-week11_34.FileManager/src/FileManager.java
@@ -10,12 +10,28 @@ import java.util.Scanner;
 public class FileManager {
 
     public List<String> read(String file) throws FileNotFoundException {
-        return null;
+        File f = new File(file);
+        Scanner scanner = new Scanner(f);
+        ArrayList<String> fileLines = new ArrayList<String>();
+        
+        while(scanner.hasNextLine()) {
+            fileLines.add(scanner.nextLine());
+        }
+        return fileLines;
     }
 
     public void save(String file, String text) throws IOException {
+        FileWriter writer = new FileWriter(file);
+        writer.write(text);
+        writer.close();
     }
 
     public void save(String file, List<String> texts) throws IOException {
+        FileWriter writer = new FileWriter(file);
+        
+        for(String line : texts) {
+            writer.write(line + "\n");
+        }
+        writer.close();
     }
 }
