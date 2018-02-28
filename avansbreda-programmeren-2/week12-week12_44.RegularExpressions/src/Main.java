b0a35b6703663c7100573a98f2af3d0ac4ec18e3
Andrew Taber <andrewtaber@me.com>
2018-02-28 08:14:08 -0700

    A bit of regex practice, nothing too involved.

--- avansbreda-programmeren-2/week12-week12_44.RegularExpressions/src/Main.java
+++ avansbreda-programmeren-2/week12-week12_44.RegularExpressions/src/Main.java
@@ -1,7 +1,47 @@
 
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
         // write test code here
+        Scanner scanner = new Scanner(System.in);
+        
+        //Just used to test user inputs
+        while(true) {
+            System.out.print("Give a string:");
+            String string = scanner.nextLine();
+            
+            if(clockTime(string)) {
+                System.out.println("The form is fine");
+            } else {
+                System.out.println("The form is wrong");
+            }
+        }
+        
+
+    }
+    
+    public static boolean isAWeekDay(String string) {
+        return string.matches("mon|tue|wed|thu|fri|sat|sun");
+    }
+    
+    public static boolean allVowels(String string) {
+        return string.matches("[aeiouäö]*");
+    }
+    
+    public static boolean clockTime(String string) {
+        if(string.matches("([0-9]{2}:){2}([0-9]{2})")) {
+            String[] hhmmss = string.split(":");
+            String hh = hhmmss[0];
+            String mm = hhmmss[1];
+            String ss = hhmmss[2];
+            
+            if(Integer.parseInt(hh) < 24 && Integer.parseInt(mm) < 60 && Integer.parseInt(ss) < 60) {
+                return true;
+            }
+        }
+        return false;
     }
 }
